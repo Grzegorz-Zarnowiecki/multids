2e3eb97c3e46d1e44fd2a9f4ea53162e474f9c3b
Grzegorz Żarnowiecki <g.zarnowiecki@gmail.com>
2018-10-29 12:09:25 +0100

    Initial commit

---
+++ src/main/java/net/gzarnowiecki/multids/MultidsApplication.java
@@ -0,0 +1,12 @@
+package net.gzarnowiecki.multids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultidsApplication.class, args);
+    }
+}
---
+++ src/test/java/net/gzarnowiecki/multids/MultidsApplicationTests.java
@@ -0,0 +1,16 @@
+package net.gzarnowiecki.multids;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultidsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
