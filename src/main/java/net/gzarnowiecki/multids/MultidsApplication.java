dd481c605e195fa4ddb0a84437b70194c1376af0
Grzegorz Żarnowiecki <g.zarnowiecki@gmail.com>
2018-10-29 19:36:23 +0100

    Disable Spring Auto configuration

--- src/main/java/net/gzarnowiecki/multids/MultidsApplication.java
+++ src/main/java/net/gzarnowiecki/multids/MultidsApplication.java
@@ -1,9 +1,12 @@
 package net.gzarnowiecki.multids;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class MultidsApplication {
 
     public static void main(String[] args) {
